b63d764e72c6308005b57a7f4db5b8dd6eda4649
tao <1445709137@qq.com>
2023-09-26 23:07:28 +0800

    🎉 init(实现create命令（创建模版）):

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+const helpOptions = require('./lib/core/help.js');
+const { createProject } = require('./lib/core/actions.js');
+
+// 定义显示模块的版本号
+program.version(require('./package.json').version);
+
+// 定义help指令
+helpOptions()
+
+program
+  .command('create <project> [others...]')
+  .description('clone a repository into a folder')
+  .action(createProject)
+
+
+// 解析终端指令
+program.parse(process.argv);
+
